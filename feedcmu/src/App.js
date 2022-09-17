d344477c2e936fdc499fe15a8685c91f5790c02b
Sanjana Shriram <sshriram@andrew.cmu.edu>
2022-09-17 13:45:24 -0400

    Add addNewLocation function for when users press submit

--- feedcmu/src/App.js
+++ feedcmu/src/App.js
@@ -6,6 +6,7 @@ import { useState } from 'react';
 import { Select, MenuItem, FormControl, InputLabel, FormHelperText, TextField } from '@mui/material';
 
 function App() {
+  const [building, setBuilding] = useState("default")
   const [name, setName] = useState('');
 
   const handleChange = event => {
@@ -18,7 +19,17 @@ function App() {
     console.log("Building selected:", event.target.value);
   }
 
-  const [building, setBuilding] = useState("default")
+  const addNewLocation = event => {
+    console.log(building)
+    const locArray = building.split(",");
+    console.log("loc array", locArray);
+    const lat = parseFloat(locArray[0]);
+    const lng = parseFloat(locArray[1]);
+    alert(lat);
+    alert(lng);
+    centers.push({ lat: lat, lng: lng });
+    console.log(centers);
+  }
 
 
   const containerStyle = {
@@ -27,7 +38,7 @@ function App() {
     justifyContent: 'center',
   };
 
-  const centers = [
+  let centers = [
     {
       lat: 40.443336,
       lng: -79.944023
@@ -196,39 +207,37 @@ function App() {
                   label="Building"
                   onChange={updateBuilding}
                 >
-                  {/* TODO add building namesss */}
-                  {/* <MenuItem value=""><em>Select Building</em></MenuItem> */}
-                  <MenuItem value={"(40.444747, -79.942084)"}>Alumni House</MenuItem>
-                  <MenuItem value={"(40.441967, -79.946668)"}>ANSYS Hall</MenuItem>
-                  <MenuItem value={"(40.441520, -79.945125)"}>Baker Hall</MenuItem>
-                  <MenuItem value={"(40.445978, -79.941064)"}>Bramer House</MenuItem>
-                  <MenuItem value={"(40.443545, -79.941982)"}>Cohon University Center</MenuItem>
-                  <MenuItem value={"(40.441534, -79.942926)"}>College of Fine Arts</MenuItem>
-                  <MenuItem value={"(40.444241, -79.943959)"}>Cyert Hall</MenuItem>
-                  <MenuItem value={"(40.442508, -79.944632)"}>Doherty Hall</MenuItem>
-                  <MenuItem value={"(40.443334, -79.946405)"}>Facilities Management Services Bldg</MenuItem>
-                  <MenuItem value={"(40.443551, -79.944519)"}>Gates Center for Computer Science</MenuItem>
-                  <MenuItem value={"(40.440897, -79.942548)"}>Hall of the Arts</MenuItem>
-                  <MenuItem value={"(40.444208, -79.945552)"}>Hamburg Hall</MenuItem>
-                  <MenuItem value={"(40.442365, -79.946670)"}>Hamerschlag Hall</MenuItem>
-                  <MenuItem value={"(40.441103, -79.943757)"}>Hunt Library</MenuItem>
-                  <MenuItem value={"(40.442024, -79.941461)"}>Margaret Morrison Carnegie Hall</MenuItem>
-                  <MenuItem value={"(40.444020, -79.946512)"}>Mehrabian Collaborative Innovation Center</MenuItem>
-                  <MenuItem value={"(40.446192, -79.951099)"}>Mellon Institute</MenuItem>
-                  <MenuItem value={"(40.443385, -79.945608)"}>Newell-Simon Hall</MenuItem>
-                  <MenuItem value={"(40.441726, -79.946346)"}>Porter Hall</MenuItem>
-                  <MenuItem value={"(40.441366, -79.942119)"}>Posner Hall</MenuItem>
-                  <MenuItem value={"(40.443508, -79.943543)"}>Purnell Center for the Arts</MenuItem>
-                  <MenuItem value={"(40.442465, -79.947255)"}>Roberts Engineering Hall</MenuItem>
-                  <MenuItem value={"(40.443002, -79.946772)"}>Scott Hall</MenuItem>
-                  <MenuItem value={"(40.440687, -79.941389)"}>Skibo Gymnasium</MenuItem>
-                  <MenuItem value={"(40.443963, -79.945536)"}>Smith Hall</MenuItem>
-                  <MenuItem value={"(40.444902, -79.947220)"}>TCS Hall</MenuItem>
-                  <MenuItem value={"(40.445167, -79.945275)"}>Tepper Building</MenuItem>
-                  <MenuItem value={"(40.444163, -79.943382)"}>Warner Hall</MenuItem>
-                  <MenuItem value={"(40.442655, -79.945683)"}>Wean Hall</MenuItem>
-                  <MenuItem value={"(40.442687, -79.940901)"}>West Wing</MenuItem>
-                  <MenuItem value={"(40.446737, -79.944693)"}>WQED Building</MenuItem>
+                  <MenuItem value={"40.444747, -79.942084"}>Alumni House</MenuItem>
+                  <MenuItem value={"40.441967, -79.946668"}>ANSYS Hall</MenuItem>
+                  <MenuItem value={"40.441520, -79.945125"}>Baker Hall</MenuItem>
+                  <MenuItem value={"40.445978, -79.941064"}>Bramer House</MenuItem>
+                  <MenuItem value={"40.443545, -79.941982"}>Cohon University Center</MenuItem>
+                  <MenuItem value={"40.441534, -79.942926"}>College of Fine Arts</MenuItem>
+                  <MenuItem value={"40.444241, -79.943959"}>Cyert Hall</MenuItem>
+                  <MenuItem value={"40.442508, -79.944632"}>Doherty Hall</MenuItem>
+                  <MenuItem value={"40.443334, -79.946405"}>Facilities Management Services Bldg</MenuItem>
+                  <MenuItem value={"40.443551, -79.944519"}>Gates Center for Computer Science</MenuItem>
+                  <MenuItem value={"40.440897, -79.942548"}>Hall of the Arts</MenuItem>
+                  <MenuItem value={"40.444208, -79.945552"}>Hamburg Hall</MenuItem>
+                  <MenuItem value={"40.442365, -79.946670"}>Hamerschlag Hall</MenuItem>
+                  <MenuItem value={"40.441103, -79.943757"}>Hunt Library</MenuItem>
+                  <MenuItem value={"40.442024, -79.941461"}>Margaret Morrison Carnegie Hall</MenuItem>
+                  <MenuItem value={"40.444020, -79.946512"}>Mehrabian Collaborative Innovation Center</MenuItem>
+                  <MenuItem value={"40.446192, -79.951099"}>Mellon Institute</MenuItem>
+                  <MenuItem value={"40.443385, -79.945608"}>Newell-Simon Hall</MenuItem>
+                  <MenuItem value={"40.441726, -79.946346"}>Porter Hall</MenuItem>
+                  <MenuItem value={"40.441366, -79.942119"}>Posner Hall</MenuItem>
+                  <MenuItem value={"40.443508, -79.943543"}>Purnell Center for the Arts</MenuItem>
+                  <MenuItem value={"40.442465, -79.947255"}>Roberts Engineering Hall</MenuItem>
+                  <MenuItem value={"40.443002, -79.946772"}>Scott Hall</MenuItem>
+                  <MenuItem value={"40.440687, -79.941389"}>Skibo Gymnasium</MenuItem>
+                  <MenuItem value={"40.443963, -79.945536"}>Smith Hall</MenuItem>
+                  <MenuItem value={"40.444902, -79.947220"}>TCS Hall</MenuItem>
+                  <MenuItem value={"40.445167, -79.945275"}>Tepper Building</MenuItem>
+                  <MenuItem value={"40.444163, -79.943382"}>Warner Hall</MenuItem>
+                  <MenuItem value={"40.442655, -79.945683"}>Wean Hall</MenuItem>
+                  <MenuItem value={"40.442687, -79.940901"}>West Wing</MenuItem>
+                  <MenuItem value={"40.446737, -79.944693"}>WQED Building</MenuItem>
                 </Select>
                 <FormHelperText>Building where free food can be picked up</FormHelperText>
               </FormControl>
@@ -243,7 +252,7 @@ function App() {
 
           </div>
           <div id="submit">
-            <Button variant="contained" color="success">Submit</Button>
+            <Button variant="contained" color="success" onClick={addNewLocation}>Submit</Button>
           </div>
 
         </div>
