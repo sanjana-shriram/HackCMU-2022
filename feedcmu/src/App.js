06e322e5d657c9bd0abc0815d6d2fb6f2ce79239
Jack-Kiefer <jackkief@gmail.com>
2022-09-17 10:50:07 -0400

    building names

--- feedcmu/src/App.js
+++ feedcmu/src/App.js
@@ -34,9 +34,46 @@ function App() {
             onChange={handleChange}
           >
             {/* TODO add building namesss */}
-            <MenuItem value={"HH"}>Ten</MenuItem>
-            <MenuItem value={""}>Twenty</MenuItem>
-            <MenuItem value={30}>Thirty</MenuItem>
+            <MenuItem value={"AH"}>Alumni House</MenuItem>
+            <MenuItem value={"AN"}>ANSYS Hall</MenuItem>
+            <MenuItem value={"BH"}>Baker Hall</MenuItem>
+            <MenuItem value={"BR"}>Bramer House</MenuItem>
+            <MenuItem value={"CUC"}>Cohon University Center</MenuItem>
+            <MenuItem value={"CFA"}>College of Fine Arts</MenuItem>
+            <MenuItem value={"CYH"}>Cyert Hall</MenuItem>
+            <MenuItem value={"DH"}>Doherty Hall</MenuItem>
+            <MenuItem value={"FM"}>Facilities Management Services Bldg</MenuItem>
+            <MenuItem value={"GHC"}>Gates Center for Computer Science</MenuItem>
+            <MenuItem value={"HOA"}>Hall of the Arts</MenuItem>
+            <MenuItem value={"HBH"}>Hamburg Hall</MenuItem>
+            <MenuItem value={"HH"}>Hamerschlag Hall</MenuItem>
+            <MenuItem value={"HL"}>Hunt Library</MenuItem>
+            <MenuItem value={"III"}>Integrated Innovation Institute</MenuItem>
+            <MenuItem value={"MM"}>Margaret Morrison Carnegie Hall</MenuItem>
+            <MenuItem value={"CIC"}>Mehrabian Collaborative Innovation Center</MenuItem>
+            <MenuItem value={"MI"}>Mellon Institute</MenuItem>
+            <MenuItem value={"M19"}>Mill 19</MenuItem>
+            <MenuItem value={"NR"}>National Robotics Engineering Center</MenuItem>
+            <MenuItem value={"NSH"}>Newell-Simon Hall</MenuItem>
+            <MenuItem value={"PTC"}>Pittsburgh Technology Center</MenuItem>
+            <MenuItem value={"PH"}>Porter Hall</MenuItem>
+            <MenuItem value={"PC"}>Posner Center</MenuItem>
+            <MenuItem value={"POS"}>Posner Hall</MenuItem>
+            <MenuItem value={"PCA"}>Purnell Center for the Arts</MenuItem>
+            <MenuItem value={"RA"}>Rand Building</MenuItem>
+            <MenuItem value={"REH"}>Roberts Engineering Hall</MenuItem>
+            <MenuItem value={"SH"}>Scaife Hall</MenuItem>
+            <MenuItem value={"SC"}>Scott Hall</MenuItem>
+            <MenuItem value={"GYM"}>Skibo Gymnasium</MenuItem>
+            <MenuItem value={"EDS"}>Smith Hall</MenuItem>
+            <MenuItem value={"SEI"}>Software Engineering Institute</MenuItem>
+            <MenuItem value={"TCS"}>TCS Hall</MenuItem>
+            <MenuItem value={"TEP"}>Tepper Building</MenuItem>
+            <MenuItem value={"WH"}>Warner Hall</MenuItem>
+            <MenuItem value={"WEH"}>Wean Hall</MenuItem>
+            <MenuItem value={"WWG"}>West Wing</MenuItem>
+            <MenuItem value={"WF"}>Whitfield Hall</MenuItem>
+            <MenuItem value={"WQ"}>WQED Building</MenuItem>
           </Select>
         </div>
 
