fb89243a5d12172665a847d631c27d4d37152fab
Sanjana Shriram <sshriram@andrew.cmu.edu>
2022-09-17 01:50:54 -0400

    Frontend inputs and submit button. Lacking functionality.

--- feedcmu/src/App.js
+++ feedcmu/src/App.js
@@ -1,25 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
+import { useState } from 'react';
+
+const submitFunction = () => {
+  console.log("submit button clicked");
+}
+
+
 
 function App() {
+  const [name, setName] = useState('');
+
+  const handleChange = event => {
+    setName(event.target.value);
+    console.log('value is:', event.target.value);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Enter details about the free food you'd like to submit to our platform using the form below.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+        <div id="form">
+          <form>
+            <label>
+              <div id="name_block">
+                Name:
+                <input type="text" name="name" onChange={handleChange} />
+              </div>
+              <div id="building_block">
+                Building:
+                <input type="text" name="building" onChange={handleChange} />
+              </div>
+              <div id="room_number_block">
+                Room Number:
+                <input type="text" name="Room Number" onChange={handleChange} />
+              </div>
+            </label>
+            <input type="submit" value="Submit" onClick={submitFunction()} />
+          </form>
+
+        </div>
+
+      </header >
+    </div >
   );
 }
 
